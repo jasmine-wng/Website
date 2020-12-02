346f739b115b88b3d39c84f47ce0849e79845977
jasminew0528-work <53498627+jasminew0528-work@users.noreply.github.com>
2020-12-02 12:24:24 -0800

    added json stuff

--- public/main.js
+++ public/main.js
@@ -33,4 +33,24 @@ $('#jam_button').click(function() {
       $("#jam_content").html(result);
     }
   });
-});
+
+function requestBlogArticlesData (){
+  $.ajax({
+    dataType: "json",
+    url: "https://firebasestorage.googleapis.com/v0/b/advancedtopicscs.appspot.com/o/blogArticles.json?alt=media&token=478d06a6-d7e5-4cac-a92d-7670dea8de17",
+    success: loadBlogArticlesJSONAsHTML,
+  });
+}
+
+function loadBlogArticlesJSONAsHTML(result) {
+  console.log(result);
+  var myString = "";
+  for (var i = 0; i < result.length; i++) {
+    var nextResult = result[i];
+
+    myString += "<h2>" + nextResult['type'] + "</h2>";
+  }
+  // Set the HTML
+  $("#blogArticles").html(myString);
+}
+$("#blogArticlesButton").click (requestBlogArticlesData);
